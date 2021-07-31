17637267feaad795b860e29fe0e4fd89d54bf12d
LuisAntonioLAG <lag.luisantonio@gmail.com>
2021-07-31 20:57:09 -0300

    adequação do Dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import {} from 'dotenv/config'
+import dotenv from 'dotenv'
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -8,16 +8,14 @@ import cors from 'cors';
 import postRoutes from './routes/contatos.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-console.log(process.env.PORT);
 
-const CONNECTION_URL = 'mongodb+srv://LuisAntonioLAG:hnn023200@cluster0.bkpko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
