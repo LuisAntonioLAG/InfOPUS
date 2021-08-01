65c2b12318dc5997133a7796d34ec553b337f99d
LuisAntonioLAG <lag.luisantonio@gmail.com>
2021-08-01 14:57:35 -0300

    organização do código

--- client/src/components/Interface/Interface.js
+++ client/src/components/Interface/Interface.js
@@ -7,12 +7,12 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 
-
 import LogoGrandeBranco from '../../assets/images/LogoGrandeBranco.png'
 import Routes from '../../Routes.js'
 
 import { CustomThemeContext } from '../../assets/themes/CustomThemeProvider'
 import { useStyles, MenuItemTooltip, MenuItem} from './Interface.styles.js';
+
 
 const Interface = props => {
     const {
@@ -24,12 +24,13 @@ const Interface = props => {
     const isDark = Boolean(currentTheme === 'GreenDarkTheme')
 
 
-        //states
+    //states
 
-        const [open, setOpen] = useState(false);
-        const [selectedListItem, setSelectedListItem] = useState();
+    const [open, setOpen] = useState(false);
+    const [selectedListItem, setSelectedListItem] = useState();
     
 
+    //Effects
 
     useLayoutEffect(() => {
       const path = window.location.pathname;
@@ -42,10 +43,6 @@ const Interface = props => {
     }, [menuItems])
 
 
-
-
-    
-
     //handlers
 
     const handleThemeChange = (event) => {
@@ -57,12 +54,9 @@ const Interface = props => {
       }
     }
 
-
-
     const handleListItemClick = (event, index) => {
       setSelectedListItem(index);
     };
-
     
     const handleDrawerOpen = () => {
       setOpen(true);
@@ -74,133 +68,124 @@ const Interface = props => {
 
 
     return(
-        <div className={classes.root}>
-            <AppBar color='primary' className={clsx(classes.appBar, {[classes.appBarShift]: open,})} >
-                <Toolbar >
+      <div className={classes.root}>
+        <AppBar color='primary' className={clsx(classes.appBar, {[classes.appBarShift]: open,})} >
+          <Toolbar >
 
+            <IconButton onClick={handleDrawerOpen} edge = 'start' className={clsx(classes.menuButton, {[classes.hide]: open,})} aria-label="menu"> <MenuIcon /> </IconButton>
+                    
+            <Typography style={{ flex: 1 }} variant="h6" noWrap>InfOPUS</Typography>
 
-                    <IconButton onClick={handleDrawerOpen} edge = 'start' className={clsx(classes.menuButton, {[classes.hide]: open,})} aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography style={{ flex: 1 }} variant="h6" noWrap>InfOPUS</Typography>
+            <FormControlLabel
+              control={
+                <Switch
+                  size='small'
+                  checked={isDark}
+                  onChange={handleThemeChange}
+                />
+              }
+              label="Modo Escuro"
+              labelPlacement="bottom"
+            />
 
-            
+            <Avatar/>
 
-                    <FormControlLabel
-                      control={<Switch
-                                  size='small'
-                                  checked={isDark}
-                                  onChange={handleThemeChange}
-                                  name="Modo Escuro"
-                              />
-                      }
-                    label="Modo Escuro"
-                    labelPlacement="bottom"
-                    />
+            <IconButton disableRipple> <ExitToAppIcon /> </IconButton>
 
+          </Toolbar>
+           
+        </AppBar>
 
-                        <Avatar/>
-                        <IconButton disableRipple>
-                            <ExitToAppIcon />
-                        </IconButton>
-
-                </Toolbar>
-            </AppBar>
-
-            <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose} className={classes.listItemIcon} >
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-
-
-        <Container style={{display: 'flex', justifyContent: 'center', padding: '20px 0'}}>
-        <img className={classes.logoside} alt='Logo da OPUS' src={LogoGrandeBranco}/>
-        </Container>
-
-        <List component="nav">
-            {menuItems.slice(0, 2).map((item, index) => (
-                <React.Fragment key={item.name}>
-                <Link className={classes.link} to={item.to}>
-                <MenuItemTooltip interactive placement="right" title={open===false ? item.txtname : ''} TransitionComponent={Zoom}>
-                <MenuItem disableRipple button selected={selectedListItem === item.name} onClick={(event) => handleListItemClick(event, item.name)}>
-                <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.txtname} />
-                </MenuItem>
-                </MenuItemTooltip>
-                </Link>
-                </React.Fragment>
-            ))}
-        </List>
-        <Divider />
-        <List component="nav">
-            {menuItems.slice(2,8).map((item, index) => (
-                <React.Fragment key={item.name}>
-                <Link className={classes.link} to={item.to}>
-                <MenuItemTooltip interactive placement="right" title={open===false ? item.txtname : ''} TransitionComponent={Zoom}>
-                <MenuItem disableRipple button selected={selectedListItem === item.name}
-                 onClick={(event) => handleListItemClick(event, item.name)}>
-                <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.txtname} />
-                </MenuItem>
-                </MenuItemTooltip>
-                </Link>
-                </React.Fragment>
-            ))}
-        </List>
-        <Divider/>
-        <List>
-            {menuItems.slice(8).map((item, index) => (
-                <React.Fragment key={item.name}>
-                <Link className={classes.link} to={item.to}>
-                <MenuItemTooltip interactive placement="right" title={open===false ? item.txtname : ''} TransitionComponent={Zoom}>
-                <MenuItem disableRipple button selected={selectedListItem === item.name}
-                 onClick={(event) => handleListItemClick(event, item.name)}>
-                <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.txtname} />
-                </MenuItem>
-                </MenuItemTooltip>
-                </Link>
-                </React.Fragment>
-                
-            ))}
-        </List>
-        <Divider />
-
-        <Typography style={{ paddingTop: "20px" }} align="center" variant="caption" >
-          InfOPUS
-        </Typography>
-        <Typography style={{ paddingBottom: "20px" }} align="center" variant="caption"
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
         >
-          v1.0.0
-        </Typography>     
+
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose} className={classes.listItemIcon} > <ChevronLeftIcon/> </IconButton>
+        
+          </div>
+
+          <Divider />
+
+
+          <Container style={{display: 'flex', justifyContent: 'center', padding: '20px 0'}}> <img className={classes.logoside} alt='Logo da OPUS' src={LogoGrandeBranco}/> </Container>
+
+          <List component="nav">
+            {menuItems.slice(0, 2).map((item, index) => (
+              <React.Fragment key={item.name}>
+                <Link className={classes.link} to={item.to}>
+                  <MenuItemTooltip interactive placement="right" title={open===false ? item.txtname : ''} TransitionComponent={Zoom}>
+                    <MenuItem disableRipple button selected={selectedListItem === item.name} onClick={(event) => handleListItemClick(event, item.name)}>
+                      <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
+                      <ListItemText primary={item.txtname} />
+                    </MenuItem>
+                  </MenuItemTooltip>
+                </Link>
+              </React.Fragment>
+            ))}
+          </List>
+
+          <Divider />
+
+          <List component="nav">
+            {menuItems.slice(2,8).map((item, index) => (
+              <React.Fragment key={item.name}>
+                <Link className={classes.link} to={item.to}>
+                  <MenuItemTooltip interactive placement="right" title={open===false ? item.txtname : ''} TransitionComponent={Zoom}>
+                    <MenuItem disableRipple button selected={selectedListItem === item.name} onClick={(event) => handleListItemClick(event, item.name)}>
+                      <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
+                      <ListItemText primary={item.txtname} />
+                    </MenuItem>
+                  </MenuItemTooltip>
+                </Link>
+              </React.Fragment>
+            ))}
+          </List>
+
+          <Divider/>
+
+          <List>
+            {menuItems.slice(8).map((item, index) => (
+              <React.Fragment key={item.name}>
+                <Link className={classes.link} to={item.to}>
+                   <MenuItemTooltip interactive placement="right" title={open===false ? item.txtname : ''} TransitionComponent={Zoom}>
+                    <MenuItem disableRipple button selected={selectedListItem === item.name} onClick={(event) => handleListItemClick(event, item.name)}>
+                      <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
+                      <ListItemText primary={item.txtname} />
+                    </MenuItem>
+                  </MenuItemTooltip>
+                </Link>
+              </React.Fragment> 
+            ))}
+          </List>
+
+          <Divider />
+
+          <Typography style={{ paddingTop: "20px" }} align="center" variant="caption" > InfOPUS </Typography>
+          <Typography style={{ paddingBottom: "20px" }} align="center" variant="caption"> v1.0.0 </Typography>     
 
 
 
-      </Drawer>
+        </Drawer>
 
         
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Paper className={classes.paper} elevation={4}>
-            <Routes/>
-        </Paper>
-      </main>
-    </div>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+            <Paper className={classes.paper} elevation={4}>
+                <Routes/>
+            </Paper>
+        </main>
+      </div>
     );
 }
 
