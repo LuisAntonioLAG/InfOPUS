acef02a589a86134a43ff5f2853acd083b2c77e1
LuisAntonioLAG <lag.luisantonio@gmail.com>
2021-08-02 00:41:39 -0300

    limpeza de código

--- client/src/components/Interface/Interface.styles.js
+++ client/src/components/Interface/Interface.styles.js
@@ -1,6 +1,5 @@
-import { makeStyles, alpha} from '@material-ui/core/styles';
+import { makeStyles} from '@material-ui/core/styles';
 
-const drawerWidth = 240;
 
 export const useStyles = makeStyles((theme) => ({
   '@global': {
