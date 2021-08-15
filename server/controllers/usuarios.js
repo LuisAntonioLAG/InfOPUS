ebc0a59f87f87d29b3bffe0e86c1d0262d23e1f7
LuisAntonioLAG <lag.luisantonio@gmail.com>
2021-08-15 10:38:00 -0300

    mudança de json para send nos erros de login

--- server/controllers/usuarios.js
+++ server/controllers/usuarios.js
@@ -11,11 +11,11 @@ export const logar = async (req, res) => {
     try {
         const usuarioExistente = await ModeloUsuario.findOne({ email });
 
-        if (!usuarioExistente) return res.status(404).json({ message: "E-mail não cadastrado" });
+        if (!usuarioExistente) return res.status(404).send({ message: "E-mail não cadastrado" });
 
         const isSenhaCorreta = await bcrypt.compare(senha, usuarioExistente.senha);
     
-        if (!isSenhaCorreta) return res.status(400).json({ message: "Senha incorreta" });
+        if (!isSenhaCorreta) return res.status(400).send({ message: "Senha incorreta" });
     
         const token = jwt.sign({ email: usuarioExistente.email, id: usuarioExistente._id }, secret);
     
@@ -33,19 +33,19 @@ export const cadastrar = async (req, res) => {
     try {
         const usuarioExistente = await ModeloUsuario.findOne({ email });
 
-        if (usuarioExistente) return res.status(400).json({ message: "E-mail já cadastrado" });
+        if (usuarioExistente) return res.status(400).send({ message: "E-mail já cadastrado" });
 
-        if (senha !== confirmSenha) return res.status(400).json({ message: "As senhas devem ser iguais" });
+        if (senha !== confirmSenha) return res.status(400).send({ message: "As senhas devem ser iguais" });
 
         const hashedSenha = await bcrypt.hash(senha, 12);
 
         const result = await ModeloUsuario.create( { email, senha: hashedSenha, nome, foto } );
 
         const token = jwt.sign({ email: result, id: result._id }, secret);
-
+        
         res.status(200).json({ result: result, token });
 
     } catch (error) {
-        res.status(500).json({ message: "Algo deu errado..." });
+        res.status(500).send({ message: "Algo deu errado..." });
     }
 }
