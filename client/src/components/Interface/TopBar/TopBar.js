c11e0da219281e7e3210fcae8feb7c942f08259d
LuisAntonioLAG <lag.luisantonio@gmail.com>
2021-08-17 17:14:14 -0300

    Mudando Theme Slide para Theme Button

--- client/src/components/Interface/TopBar/TopBar.js
+++ client/src/components/Interface/TopBar/TopBar.js
@@ -1,13 +1,14 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import clsx from 'clsx';
 import { Route } from 'react-router';
-import {AppBar, Link, Box, Breadcrumbs, Toolbar, Switch, IconButton, Hidden, Typography, Avatar, FormControlLabel} from '@material-ui/core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {AppBar, Link, Box, Breadcrumbs, Toolbar, IconButton, Hidden, Typography, Avatar} from '@material-ui/core'
 
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import { CustomThemeContext } from '../../../assets/themes/CustomThemeProvider.js';
@@ -27,29 +28,15 @@ const TopBar = props => {
     const dispatch = useDispatch();
     const history = useHistory();
     const LinkRouter = (props) => <Link {...props} component={RouterLink} />;
-    const [espessura, setEspessura] = useState(window.outerWidth)
 
 
-    useEffect(() => {
-        const updateWindowWidth = () => {
-          setEspessura(window.outerWidth)
-        }
-    
-        window.addEventListener('resize', updateWindowWidth);
-    
-        return () => window.removeEventListener('resize', updateWindowWidth);
-      }, []);
-
-
-
-    const handleThemeChange = (event) => {
-        const { checked } = event.target
-        if (checked) {
-          setTheme('GreenDarkTheme')
-        } else {
-          setTheme('GreenLightTheme')
-        }
+    const handleThemeButton = () => {
+      if (currentTheme === 'GreenDarkTheme') {
+        setTheme('GreenLightTheme')
+      } else {
+        setTheme('GreenDarkTheme')
       }
+    };
 
 
     const handleDrawerOpen = () => {
@@ -57,7 +44,6 @@ const TopBar = props => {
     };
 
     const logout = () => {
-      dispatch( {type: 'LOGOUT'});
       history.push('/login');
       sessionStorage.clear();
     }
@@ -101,27 +87,17 @@ const TopBar = props => {
         </Route>
         </Hidden>
 
+            
         <Typography style={{ flex: 1 }} variant="h6" noWrap>InfOPUS</Typography>
-        
+          
 
-          <FormControlLabel
-            control={
-              <Switch
-                size='small'
-                checked={isDark}
-                onChange={handleThemeChange}
-              />
-            }
-            label={espessura>960 ? "Modo Escuro" : ''}
-            labelPlacement="bottom"
-          />
+        <IconButton   onClick={handleThemeButton}> {isDark ? <BrightnessHighIcon/> : <Brightness4Icon/>} </IconButton>
 
-          <Box px={1}>
-          <Avatar className={classes.small} src={user?.result.foto} alt={user?.result.nome}>{user?.result.nome.charAt(0)}</Avatar>
-          <Hidden smDown><Typography >{user?.result.nome}</Typography></Hidden>
+        <Box px={1}>
+          <Avatar  src={user?.result.foto} alt={user?.result.nome}>{user?.result.nome.charAt(0)}</Avatar>
           </Box>
 
-          <IconButton size='small' onClick={logout}> <FontAwesomeIcon className={classes.titleIcon} icon={faSignOutAlt} /> </IconButton>
+          <IconButton onClick={logout}> <ExitToAppIcon/> </IconButton>
 
         </Toolbar>
          
